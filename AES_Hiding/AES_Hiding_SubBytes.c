e7b76e1694b6b60958e428beb502c35fcc166afa
Jaygg23 <150669389+Jaygg23@users.noreply.github.com>
2025-11-08 01:19:05 +0900

    Add files via upload

---
+++ AES_Hiding/AES_Hiding_SubBytes.c
@@ -0,0 +1,285 @@
+﻿#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+#include <stdint.h>
+#include <math.h>
+#include <string.h>
+
+//#define xtimes(input) (((input)<<1) ^ (((input)>>7) * 0x1b)) // 함수
+#define xtimes(f) ((((f) >> 7 & 0x01) == 1) ? ((f) << 1) ^ 0x1b : (f) << 1) // 분기문
+
+#define dummy_num 4 // 추가할 더미 개수
+
+typedef uint8_t AES_STATE_t[18]; // 128-bit block
+typedef uint8_t AES128_KEY_t[16]; // 128-bit masterkey
+
+const unsigned char s_box[256] = {
+        0x63, 0x7c, 0x77, 0x7b, 0xf2, 0x6b, 0x6f, 0xc5, 0x30, 0x01, 0x67, 0x2b, 0xfe, 0xd7, 0xab, 0x76,
+        0xca, 0x82, 0xc9, 0x7d, 0xfa, 0x59, 0x47, 0xf0, 0xad, 0xd4, 0xa2, 0xaf, 0x9c, 0xa4, 0x72, 0xc0,
+        0xb7, 0xfd, 0x93, 0x26, 0x36, 0x3f, 0xf7, 0xcc, 0x34, 0xa5, 0xe5, 0xf1, 0x71, 0xd8, 0x31, 0x15,
+        0x04, 0xc7, 0x23, 0xc3, 0x18, 0x96, 0x05, 0x9a, 0x07, 0x12, 0x80, 0xe2, 0xeb, 0x27, 0xb2, 0x75,
+        0x09, 0x83, 0x2c, 0x1a, 0x1b, 0x6e, 0x5a, 0xa0, 0x52, 0x3b, 0xd6, 0xb3, 0x29, 0xe3, 0x2f, 0x84,
+        0x53, 0xd1, 0x00, 0xed, 0x20, 0xfc, 0xb1, 0x5b, 0x6a, 0xcb, 0xbe, 0x39, 0x4a, 0x4c, 0x58, 0xcf,
+        0xd0, 0xef, 0xaa, 0xfb, 0x43, 0x4d, 0x33, 0x85, 0x45, 0xf9, 0x02, 0x7f, 0x50, 0x3c, 0x9f, 0xa8,
+        0x51, 0xa3, 0x40, 0x8f, 0x92, 0x9d, 0x38, 0xf5, 0xbc, 0xb6, 0xda, 0x21, 0x10, 0xff, 0xf3, 0xd2,
+        0xcd, 0x0c, 0x13, 0xec, 0x5f, 0x97, 0x44, 0x17, 0xc4, 0xa7, 0x7e, 0x3d, 0x64, 0x5d, 0x19, 0x73,
+        0x60, 0x81, 0x4f, 0xdc, 0x22, 0x2a, 0x90, 0x88, 0x46, 0xee, 0xb8, 0x14, 0xde, 0x5e, 0x0b, 0xdb,
+        0xe0, 0x32, 0x3a, 0x0a, 0x49, 0x06, 0x24, 0x5c, 0xc2, 0xd3, 0xac, 0x62, 0x91, 0x95, 0xe4, 0x79,
+        0xe7, 0xc8, 0x37, 0x6d, 0x8d, 0xd5, 0x4e, 0xa9, 0x6c, 0x56, 0xf4, 0xea, 0x65, 0x7a, 0xae, 0x08,
+        0xba, 0x78, 0x25, 0x2e, 0x1c, 0xa6, 0xb4, 0xc6, 0xe8, 0xdd, 0x74, 0x1f, 0x4b, 0xbd, 0x8b, 0x8a,
+        0x70, 0x3e, 0xb5, 0x66, 0x48, 0x03, 0xf6, 0x0e, 0x61, 0x35, 0x57, 0xb9, 0x86, 0xc1, 0x1d, 0x9e,
+        0xe1, 0xf8, 0x98, 0x11, 0x69, 0xd9, 0x8e, 0x94, 0x9b, 0x1e, 0x87, 0xe9, 0xce, 0x55, 0x28, 0xdf,
+        0x8c, 0xa1, 0x89, 0x0d, 0xbf, 0xe6, 0x42, 0x68, 0x41, 0x99, 0x2d, 0x0f, 0xb0, 0x54, 0xbb, 0x16
+};
+
+const unsigned char RC[10] = {
+    0x01, 0x02, 0x04, 0x08, 0x10, 0x20, 0x40, 0x80, 0x1B, 0x36
+};
+
+void AddRoundKey(unsigned char string[], unsigned char key[])
+{
+    string[0] ^= key[0];
+    string[1] ^= key[1];
+    string[2] ^= key[2];
+    string[3] ^= key[3];
+    string[4] ^= key[4];
+    string[5] ^= key[5];
+    string[6] ^= key[6];
+    string[7] ^= key[7];
+    string[8] ^= key[8];
+    string[9] ^= key[9];
+    string[10] ^= key[10];
+    string[11] ^= key[11];
+    string[12] ^= key[12];
+    string[13] ^= key[13];
+    string[14] ^= key[14];
+    string[15] ^= key[15];
+}
+
+void SubBytes(unsigned char string[])
+{
+    string[0] = s_box[string[0]];
+    string[1] = s_box[string[1]];
+    string[2] = s_box[string[2]];
+    string[3] = s_box[string[3]];
+    string[4] = s_box[string[4]];
+    string[5] = s_box[string[5]];
+    string[6] = s_box[string[6]];
+    string[7] = s_box[string[7]];
+    string[8] = s_box[string[8]];
+    string[9] = s_box[string[9]];
+    string[10] = s_box[string[10]];
+    string[11] = s_box[string[11]];
+    string[12] = s_box[string[12]];
+    string[13] = s_box[string[13]];
+    string[14] = s_box[string[14]];
+    string[15] = s_box[string[15]];
+}
+
+void ShiftRows(unsigned char string[])
+{
+    unsigned char buf;
+
+    buf = string[1];
+    string[1] = string[5];
+    string[5] = string[9];
+    string[9] = string[13];
+    string[13] = buf;
+
+    buf = string[2];
+    string[2] = string[10];
+    string[10] = buf;
+    buf = string[6];
+    string[6] = string[14];
+    string[14] = buf;
+
+    buf = string[15];
+    string[15] = string[11];
+    string[11] = string[7];
+    string[7] = string[3];
+    string[3] = buf;
+}
+
+void MixColumns(unsigned char string[])
+{
+    unsigned char buf[4] = { 0x00, };
+
+    buf[0] = string[0];
+    buf[1] = string[1];
+    buf[2] = string[2];
+    buf[3] = string[3];
+    string[0] = xtimes((buf[0] ^ buf[1])) ^ buf[1] ^ buf[2] ^ buf[3];
+    string[1] = buf[0] ^ xtimes((buf[1] ^ buf[2])) ^ buf[2] ^ buf[3];
+    string[2] = buf[0] ^ buf[1] ^ xtimes((buf[2] ^ buf[3])) ^ buf[3];
+    string[3] = xtimes((buf[0] ^ buf[3])) ^ buf[0] ^ buf[1] ^ buf[2];
+
+    buf[0] = string[4];
+    buf[1] = string[5];
+    buf[2] = string[6];
+    buf[3] = string[7];
+    string[4] = xtimes((buf[0] ^ buf[1])) ^ buf[1] ^ buf[2] ^ buf[3];
+    string[5] = buf[0] ^ xtimes((buf[1] ^ buf[2])) ^ buf[2] ^ buf[3];
+    string[6] = buf[0] ^ buf[1] ^ xtimes((buf[2] ^ buf[3])) ^ buf[3];
+    string[7] = xtimes((buf[0] ^ buf[3])) ^ buf[0] ^ buf[1] ^ buf[2];
+
+    buf[0] = string[8];
+    buf[1] = string[9];
+    buf[2] = string[10];
+    buf[3] = string[11];
+    string[8] = xtimes((buf[0] ^ buf[1])) ^ buf[1] ^ buf[2] ^ buf[3];
+    string[9] = buf[0] ^ xtimes((buf[1] ^ buf[2])) ^ buf[2] ^ buf[3];
+    string[10] = buf[0] ^ buf[1] ^ xtimes((buf[2] ^ buf[3])) ^ buf[3];
+    string[11] = xtimes((buf[0] ^ buf[3])) ^ buf[0] ^ buf[1] ^ buf[2];
+
+    buf[0] = string[12];
+    buf[1] = string[13];
+    buf[2] = string[14];
+    buf[3] = string[15];
+    string[12] = xtimes((buf[0] ^ buf[1])) ^ buf[1] ^ buf[2] ^ buf[3];
+    string[13] = buf[0] ^ xtimes((buf[1] ^ buf[2])) ^ buf[2] ^ buf[3];
+    string[14] = buf[0] ^ buf[1] ^ xtimes((buf[2] ^ buf[3])) ^ buf[3];
+    string[15] = xtimes((buf[0] ^ buf[3])) ^ buf[0] ^ buf[1] ^ buf[2];
+}
+
+void KeySchedule128(unsigned char key[][16])
+{
+    int cnt_i;
+
+    for (cnt_i = 0; cnt_i < 10; cnt_i++)
+    {
+        key[cnt_i + 1][0] = key[cnt_i][0] ^ s_box[key[cnt_i][13]] ^ RC[cnt_i];
+        key[cnt_i + 1][1] = key[cnt_i][1] ^ s_box[key[cnt_i][14]];
+        key[cnt_i + 1][2] = key[cnt_i][2] ^ s_box[key[cnt_i][15]];
+        key[cnt_i + 1][3] = key[cnt_i][3] ^ s_box[key[cnt_i][12]];
+
+        key[cnt_i + 1][4] = key[cnt_i + 1][0] ^ key[cnt_i][4];
+        key[cnt_i + 1][5] = key[cnt_i + 1][1] ^ key[cnt_i][5];
+        key[cnt_i + 1][6] = key[cnt_i + 1][2] ^ key[cnt_i][6];
+        key[cnt_i + 1][7] = key[cnt_i + 1][3] ^ key[cnt_i][7];
+
+        key[cnt_i + 1][8] = key[cnt_i + 1][4] ^ key[cnt_i][8];
+        key[cnt_i + 1][9] = key[cnt_i + 1][5] ^ key[cnt_i][9];
+        key[cnt_i + 1][10] = key[cnt_i + 1][6] ^ key[cnt_i][10];
+        key[cnt_i + 1][11] = key[cnt_i + 1][7] ^ key[cnt_i][11];
+
+        key[cnt_i + 1][12] = key[cnt_i + 1][8] ^ key[cnt_i][12];
+        key[cnt_i + 1][13] = key[cnt_i + 1][9] ^ key[cnt_i][13];
+        key[cnt_i + 1][14] = key[cnt_i + 1][10] ^ key[cnt_i][14];
+        key[cnt_i + 1][15] = key[cnt_i + 1][11] ^ key[cnt_i][15];
+    }
+}
+
+/* ---------------- Hiding ---------------- */
+void Fisher_Yates_shuffle(int* arr, int size) { // Fisher–Yates 방식 셔플
+    for (int i = size - 1; i > 0; i--) {
+        int j = rand() % (i + 1); // 0 ~ i 사이 랜덤 인덱스 선택
+        int tmp = arr[i];
+        arr[i] = arr[j];
+        arr[j] = tmp;
+    }
+}
+void SubBytes_Hiding(unsigned char state[], int dummy_count, int round, int shuff_idxs[18]) {
+    int total = 18;   // 실제 16바이트 + 더미 바이트
+    //unsigned char dummy_plus_state[18];
+
+    //for (int i = 0; i < 16; i++) {
+    //    state[i] = state[i]; // state[0~15] 실제 데이터 복사
+    //}
+    for (int i = 16; i < total; i++) {
+        state[i] = (unsigned char)(0x78 ^ i); // state[16~15 + dummy_count] 더미 채우기
+    }
+    //for (int i = 0; i < total; i++) {
+    //    order[i] = indices[i]; // order 배열을 미리 정의된 패턴으로 섞기
+    //}
+    
+    printf("[Round %d | SubBytes Hiding]: ", round + 1);
+    printf(" Order: ");
+    for (int i = 0; i < total; i++) {
+        if (shuff_idxs[i] < 16) printf("%d ", shuff_idxs[i]);
+        else printf("D ");
+    }
+    printf("\n");
+    
+    // SubBytes 수행
+    for (int i = 0; i < total; i++) {
+        //int idx = shuff_idxs[i];
+        //unsigned char before = dummy_plus_state[shuff_idxs[i]];
+        state[shuff_idxs[i]] = s_box[state[shuff_idxs[i]]];
+
+        //if (idx < 16) { // 실제 데이터
+        //    printf("SubBytes[%d]: 0x%02X -> 0x%02X\n", idx, before, dummy_plus_state[idx]);
+        //}
+        //else {        // 더미 데이터
+        //    printf("   Dummy[SubBytes]: 0x%02X -> 0x%02X\n", before, dummy_plus_state[idx]);
+        //}
+    }
+
+    // 6. 변환된 실제 state[0~15]만 복사해둔다 (더미는 버림)
+    //for (int i = 0; i < 16; i++) {
+    //    state[state_indices[i]] = dummy_plus_state[state_indices[i]];
+    //}
+    
+    // printf("\n");
+}
+
+void Encrypt(AES_STATE_t PT, AES128_KEY_t KEY, AES_STATE_t CT)
+{
+    unsigned char RoundKey[11][16];
+
+    int shuff_idxs[18];
+    for (int i = 0; i < 18; i++) shuff_idxs[i] = i;
+
+    for (int i = 0; i < 16; i++)
+    {
+        RoundKey[0][i] = KEY[i];
+        CT[i] = PT[i];
+    }
+
+    KeySchedule128(RoundKey);
+
+    AddRoundKey(CT, RoundKey[0]);
+
+    //1~9 라운드
+    for (int round = 0; round < 9; round++)
+    {
+        //SubBytes(CT);
+        SubBytes_Hiding(CT, dummy_num, round, shuff_idxs);
+        ShiftRows(CT);
+        MixColumns(CT);
+        AddRoundKey(CT, RoundKey[round + 1]);
+    }
+
+    //마지막 10 라운드
+    //SubBytes(CT);
+    SubBytes_Hiding(CT, dummy_num, 9, shuff_idxs);
+    ShiftRows(CT);
+    AddRoundKey(CT, RoundKey[10]);
+}
+
+int main() {
+    //srand(12345);
+    unsigned char plaintext[16] = { // 랜덤 -> 수정!
+        0x6b, 0xc1, 0xbe, 0xe2,
+        0x2e, 0x40, 0x9f, 0x96,
+        0xe9, 0x3d, 0x7e, 0x11,
+        0x73, 0x93, 0x17, 0x2a
+    };
+
+    unsigned char key[16] = { // 고정
+        0x2b, 0x7e, 0x15, 0x16,
+        0x28, 0xae, 0xd2, 0xa6,
+        0xab, 0xf7, 0x15, 0x88,
+        0x09, 0xcf, 0x4f, 0x3c
+    };
+
+    unsigned char ciphertext[16];
+
+    Encrypt(plaintext, key, ciphertext);
+
+    printf("Ciphertext:\n");
+    for (int i = 0; i < 16; i++) {
+        printf("%02x ", ciphertext[i]);
+    }
+    printf("\n");
+
+    return 0;
+}
